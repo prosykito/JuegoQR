3caa734c204f1584e84c09faf848edeab80dd31a
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-13 20:34:16 +0200

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -9,7 +9,7 @@ const preguntas = {
         pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en mililitros, sino en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesado como el plomo.
 Buscad el Gran Juez Inanimado de San Martín de Valvení. Es un frío altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo.
 Vuestra pista no está en las alturas, sino a ras de suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallaréis vuestra salida.",
-        respuesta: "56",
+        respuesta: "3az87",
         mensaje: "correcto"
     },
     3: {
