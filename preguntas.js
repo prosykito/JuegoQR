86b858287c746cfb3db79613e8743423055038f7
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-13 21:37:07 +0200

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,40 +1,40 @@
 const preguntas = {
     1: {
     pregunta: "Los locos se obsesionan con las cifras. Necesitan que conteis el numero de cuadrados de la red de volleyball multiplicado por el numero de pájaros de la pista de pádel dividido entre el numero de barbacoas.",
-    respuesta: "3995",
-    mensaje: "¡Correcto!"
+    respuesta: "0 3995",
+    mensaje: "¡Correcto! el codigo es 2"
 },
 
     2: {
         pregunta: "El siguiente acertijo os indicará el lugar donde encontrar el código: En el ala oeste del asilo aprendimos que la locura no se mide en mililitros, sino en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesado como el plomo.Buscad el Gran Juez Inanimado de San Martín de Valvení. Es un frío altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo.Vuestra pista no está en las alturas, sino a ras de suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallaréis vuestra salida.",
-        codigo: "3az87",
-        mensaje: "correcto"
+        codigo: "2 3az87",
+        mensaje: "correcto. el codigo es 5"
     },
     3: {
         pregunta: "Escribe la fecha exacta en la que uno de nuestros pacientes dijo: San Martín guarda secretos",
-        respuesta: "28/09/82",
-        mensaje: "Muy bien.\n\nLa siguiente coordenada es B4 y el codigo es 5211"
+        respuesta: "5 28/09/82",
+        mensaje: "Muy bien.\n\nLa siguiente coordenada es B4 y el codigo es 7"
 },
 
     4: {
         pregunta:"Los ojos pueden engañarte. Confia unicamente en tus manos. Introduce la mano en la caja y descubre el diagnostico oculto",
-        respuesta: "lunatico",
-        mensaje: "Exacto"
+        respuesta: "7 lunatico",
+        mensaje: "Exacto codigo 9"
     },
     5: {
         pregunta:"En esta zona hay pacientes muy violentos y han roto el codigo de seguridad. recomponerlo para poder continuar",
-        respuesta:"s3gur1d4d",
-        mensaje:"bien"
+        respuesta:"9 s3gur1d4d",
+        mensaje:"bien codigo 5"
     },
     6: {
         pregunta:"Encuentra la clave de la tarjeta de seguridad",
-        respuesta:"1234",
-        mensaje:"exitazo"
+        respuesta:"5 1234",
+        mensaje:"exitazo codigo 4"
 
     },
     7: {
-        pregunta:"",
-        respuesta:"",
+        pregunta:"holi",
+        respuesta:"4 holi",
         mensaje:"bravisimo"
     },
 };
