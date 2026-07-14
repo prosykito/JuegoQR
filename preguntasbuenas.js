1d0a52b05ecd47f2ef40132f9b5f83ca1ca16946
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-14 20:29:10 +0200

    Update preguntasbuenas.js

--- preguntasbuenas.js
+++ preguntasbuenas.js
@@ -1,1 +1,78 @@
+const preguntas = {
+    1: {
+        pregunta: "Los locos se obsesionan con los número. Contad el número de cuadrados de la red de volleyball multiplicado por el número de pajaros de la pista de pádel y divide el resultado entre el número de barbacoas.",
+        respuesta: "3995",
+        mensaje: "",
+    
+        imagen: "",
+        video: ""
+},
 
+    2: {
+        pregunta: "Entre todos los pacientes hubo uno que repetia constantemente :'San Martín, guarda secretos' ¿En que fecha ocurrió?",
+        respuesta: "28/09/82",
+        mensaje: "",
+    
+        imagen: "",
+        video: ""
+    },
+    3: {
+        pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en milímetros, si no en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesa como el plomo. /n Buscad el gran juez inanimado de San Martín de Valvení. Es un frio altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo. /n Vuestra pista no está en las alturas, sino al ras del suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situais sobre el juez, sabréis vuestra condena; si encontrais su ranura, hallareis vuestra salida. /n El código no se esconde en el aire, si no bajo los pies del gigante que descansa.  Buscad ese lugar y hallareis el código oculto",
+        respuesta: "3az87",
+        mensaje: "Muy bien.\n\nLa siguiente coordenada es B4 y el codigo es 7",
+    
+        imagen: "",
+        video: ""
+},
+
+    4: {
+        pregunta:"cuanto es 4+4",
+        respuesta: "8",
+        mensaje: "Exacto codigo 9",
+
+        imagen: "",
+        video: ""
+    },
+    5: {
+        pregunta:"cuanto es 5+5",
+        respuesta:"10",
+        mensaje:"bien codigo 5",
+
+        imagen: "",
+        video: ""
+    },
+    6: {
+        pregunta:"cuanto es 6+6",
+        respuesta:"12",
+        mensaje:"exitazo codigo 4",
+
+        imagen: "",
+        video: ""
+
+    },
+    7: {
+        pregunta:"Cuanto es 1+1",
+        respuesta:"2",
+        mensaje:"bravisimo",
+
+        imagen: "",
+        video: ""
+    },    
+    8: {
+    pregunta:"cuanto es 2+2",
+    respuesta:"4",
+    mensaje:"exitazo codigo 4",
+
+    imagen: "",
+    video: "video_prueba.mp4"
+    },
+    9: {
+    pregunta:"4+4",
+    respuesta:"8",
+    mensaje:"exitazo codigo 4",
+
+    imagen: "",
+    video: ""
+
+    },
+};
