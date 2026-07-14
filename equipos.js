18b527089dee1a7fc24f918470e27a7ded518329
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-14 07:39:33 +0200

    Add files via upload

---
+++ equipos.js
@@ -0,0 +1,53 @@
+const equipos = {
+
+    equipo1: {
+
+        nombre: "Equipo 1",
+
+        pruebas: {
+
+            1: {
+                codigoEntrada: "0000",
+                codigoSalida: "1234",
+                coordenada: "A1"
+            },
+
+            2: {
+                codigoEntrada: "1234",
+                codigoSalida: "2365",
+                coordenada: "B2"
+            },
+
+            3: {
+                codigoEntrada: "2365",
+                codigoSalida: "7342",
+                coordenada: "C3"
+            }
+
+        }
+
+    },
+
+    equipo2: {
+
+        nombre: "Equipo 2",
+
+        pruebas: {
+
+            1: {
+                codigoEntrada: "0000",
+                codigoSalida: "8842",
+                coordenada: "D4"
+            },
+
+            2: {
+                codigoEntrada: "8842",
+                codigoSalida: "6517",
+                coordenada: "A8"
+            }
+
+        }
+
+    }
+
+};
