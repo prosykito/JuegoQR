bd11cb417cf0b3293b1b522616d005368a31d504
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-14 08:24:30 +0200

    Update equipos.js

--- equipos.js
+++ equipos.js
@@ -2,32 +2,55 @@ const equipos = {
 
     equipo1: {
 
-        nombre: "Equipo 1",
+    nombre: "Equipo 1",
 
-        pruebas: {
+    pruebas: {
 
-            1: {
-                codigoEntrada: "0000",
-                codigoSalida: "1234",
-                coordenada: "A1"
-            },
+        1: {
+            codigoEntrada: "0000",
+            codigoSalida: "1234",
+            coordenada: "A1"
+        },
 
-            2: {
-                codigoEntrada: "1234",
-                codigoSalida: "2365",
-                coordenada: "B2"
-            },
+        2: {
+            codigoEntrada: "1234",
+            codigoSalida: "2365",
+            coordenada: "B2"
+        },
 
-            3: {
-                codigoEntrada: "2365",
-                codigoSalida: "7342",
-                coordenada: "C3"
-            }
+        3: {
+            codigoEntrada: "2365",
+            codigoSalida: "7342",
+            coordenada: "C3"
+        },
 
+        4: {
+            codigoEntrada: "7342",
+            codigoSalida: "8421",
+            coordenada: "D4"
+        },
+
+        5: {
+            codigoEntrada: "8421",
+            codigoSalida: "9255",
+            coordenada: "E5"
+        },
+
+        6: {
+            codigoEntrada: "9255",
+            codigoSalida: "4310",
+            coordenada: "F6"
+        },
+
+        7: {
+            codigoEntrada: "4310",
+            codigoSalida: "8888",
+            coordenada: "FINAL"
         }
 
-    },
+    }
 
+},
     equipo2: {
 
         nombre: "Equipo 2",
