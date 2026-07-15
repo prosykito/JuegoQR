75ed55b131e5ddc51b1faf40c17f5b387712cb87
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-15 19:55:56 +0200

    Update preguntasbuenas.js

--- preguntasbuenas.js
+++ preguntasbuenas.js
@@ -6,24 +6,7 @@ const preguntas = {
     
         imagen: "",
         video: ""
-},
-
-    3: {
-        pregunta: "Entre todos los pacientes hubo uno que repetía constantemente :'San Martín, guarda secretos' ¿En qué fecha ocurrió?",
-        respuesta: "28/09/82",
-        mensaje: "",
-    
-        imagen: "",
-        video: ""
     },
-    4: {
-        pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en milímetros, si no en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesa como el plomo. Buscad el gran juez inanimado de San Martín de Valvení. Es un frio altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo. Vuestra pista no está en las alturas, sino al ras del suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallareis vuestra salida. El código no se esconde en el aire, si no bajo los pies del gigante que descansa.  Buscad ese lugar y hallareis el código oculto",
-        respuesta: "3az87",
-        mensaje: "",
-    
-        imagen: "",
-        video: ""
-},
 
     2: {
         pregunta:"Los ojos pueden engañarte asique confía únicamente en tus manos. Introduce la mano en la caja y descubre el diagnóstico oculto",
@@ -33,7 +16,17 @@ const preguntas = {
         imagen: "",
         video: ""
     },
-    5: {
+
+    3: {
+        pregunta: "Entre todos los pacientes hubo uno que repetía constantemente :'San Martín, guarda secretos' ¿En qué fecha ocurrió?",
+        respuesta: "28/09/82",
+        mensaje: "",
+    
+        imagen: "",
+        video: ""
+    },
+
+     4: {
         pregunta:"En esta zona merodean pacientes muy violentos y han roto el código de seguridad. Tendréis que recomponerlo para poder continuar",
         respuesta:"s3gur1d4d",
         mensaje:"",
@@ -41,6 +34,17 @@ const preguntas = {
         imagen: "",
         video: ""
     },
+
+     5: {
+        pregunta:"4+4",
+        respuesta:"8",
+        mensaje:"exitazo codigo 4",
+    
+        imagen: "",
+        video: ""
+
+    },
+
     6: {
         pregunta:"Esta es la última comunicación recibida de una de nuestras enfermeras que consiguió escapar a duras penas de los pacientes. Necesitamos que recuperéis su código de acceso",
         respuesta:"0089-VALV-2026",
@@ -50,6 +54,7 @@ const preguntas = {
         video: "video_prueba.mp4"
 
     },
+     
     7: {
         pregunta:"La mayoría de los pacientes olvidaron su nombre, pero jamás olvidaran una palabra. Buscad en el barrio de los quietos, entre los nombres de los que ya no están, que palabra se repite una y otra vez",
         respuesta:"familia",
@@ -58,7 +63,16 @@ const preguntas = {
         imagen: "",
         video: ""
     },    
+    
     8: {
+        pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en milímetros, si no en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesa como el plomo. Buscad el gran juez inanimado de San Martín de Valvení. Es un frio altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo. Vuestra pista no está en las alturas, sino al ras del suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallareis vuestra salida. El código no se esconde en el aire, si no bajo los pies del gigante que descansa.  Buscad ese lugar y hallareis el código oculto",
+        respuesta: "3az87",
+        mensaje: "",
+    
+        imagen: "",
+        video: ""
+    },
+    9: {
         pregunta:"La paciente 014 que escapó del psiquiátrico es la única que conoce la contraseña de la caja de seguridad del director. Necesitamos que averigüéis cual es pero solo se la dirá a aquel que conozca la palabra clave",
         respuesta:"4728",
         mensaje:"",
@@ -66,13 +80,5 @@ const preguntas = {
         imagen: "cartel_vieja.jpg",
         video: ""
     },
-    9: {
-        pregunta:"4+4",
-        respuesta:"8",
-        mensaje:"exitazo codigo 4",
     
-        imagen: "",
-        video: ""
-
-    },
 };
