7e38286bd4c1066eb3cb98acd52e295931590745
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-15 20:35:42 +0200

    Update preguntasbuenas.js

--- preguntasbuenas.js
+++ preguntasbuenas.js
@@ -1,6 +1,6 @@
 const preguntas = {
     1: {
-        pregunta: "Los locos se obsesionan con los números. Contad el número de cuadrados de la red de voleibol multiplicado por el número de pájaros de la pista de pádel y divide el resultado entre el número de barbacoas",
+        pregunta: "Los locos se obsesionan con los números. Contad el número de cuadrados de la red de voleibol multiplicado por el número de pájaros de la pista de pádel y divide el resultado entre el número de barbacoas.",
         respuesta: "3995",
         mensaje: "",
     
@@ -9,7 +9,7 @@ const preguntas = {
     },
 
     2: {
-        pregunta:"Los ojos pueden engañarte asique confía únicamente en tus manos. Introduce la mano en la caja y descubre el diagnóstico oculto",
+        pregunta:"Los ojos pueden engañarte asique confía únicamente en tus manos. Introduce la mano en la caja y descubre el diagnóstico oculto.",
         respuesta: "lunatico",
         mensaje: "",
 
@@ -27,7 +27,7 @@ const preguntas = {
     },
 
      4: {
-        pregunta:"En esta zona merodean pacientes muy violentos y han roto el código de seguridad. Tendréis que recomponerlo para poder continuar",
+        pregunta:"En esta zona merodean pacientes muy violentos y han roto el código de seguridad. Tendréis que recomponerlo para poder continuar.",
         respuesta:"s3gur1d4d",
         mensaje:"",
 
@@ -46,7 +46,7 @@ const preguntas = {
     },
 
     6: {
-        pregunta:"Esta es la última comunicación recibida de una de nuestras enfermeras que consiguió escapar a duras penas de los pacientes. Necesitamos que recuperéis su código de acceso",
+        pregunta:"Esta es la última comunicación recibida de una de nuestras enfermeras que consiguió escapar a duras penas de los pacientes. Necesitamos que recuperéis su código de acceso.",
         respuesta:"0089-VALV-2026",
         mensaje:"",
 
@@ -56,7 +56,7 @@ const preguntas = {
     },
      
     7: {
-        pregunta:"La mayoría de los pacientes olvidaron su nombre, pero jamás olvidaran una palabra. Buscad en el barrio de los quietos, entre los nombres de los que ya no están, que palabra se repite una y otra vez",
+        pregunta:"La mayoría de los pacientes olvidaron su nombre, pero jamás olvidaran una palabra. Buscad en el barrio de los quietos, entre los nombres de los que ya no están, que palabra se repite una y otra vez.",
         respuesta:"familia",
         mensaje:"",
 
@@ -65,7 +65,7 @@ const preguntas = {
     },    
     
     8: {
-        pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en milímetros, si no en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesa como el plomo. Buscad el gran juez inanimado de San Martín de Valvení. Es un frio altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo. Vuestra pista no está en las alturas, sino al ras del suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallareis vuestra salida. El código no se esconde en el aire, si no bajo los pies del gigante que descansa.  Buscad ese lugar y hallareis el código oculto",
+        pregunta: "En el ala oeste del asilo aprendimos que la locura no se mide en milímetros, si no en la presión que ejerce el alma sobre el suelo. El delirio te vuelve ligero como el humo; la culpa, pesa como el plomo. Buscad el gran juez inanimado de San Martín de Valvení. Es un frio altar gris empotrado en la tierra, diseñado para que los hombres confiesen su carga sin decir una sola palabra. Carece de ojos, pero dictamina sentencias mudas usando solo números que emergen del suelo. Vuestra pista no está en las alturas, sino al ras del suelo, allí donde los frutos de la tierra y los monstruos de hierro se ven obligados a detener su marcha para ser desvestidos de su masa. Si os situáis sobre el juez, sabréis vuestra condena; si encontráis su ranura, hallareis vuestra salida. El código no se esconde en el aire, si no bajo los pies del gigante que descansa.  Buscad ese lugar y hallareis el código oculto.",
         respuesta: "3az87",
         mensaje: "",
     
@@ -73,7 +73,7 @@ const preguntas = {
         video: ""
     },
     9: {
-        pregunta:"La paciente 014 que escapó del psiquiátrico es la única que conoce la contraseña de la caja de seguridad del director. Necesitamos que averigüéis cual es pero solo se la dirá a aquel que conozca la palabra clave",
+        pregunta:"La paciente 014 que escapó del psiquiátrico es la única que conoce la contraseña de la caja de seguridad del director. Necesitamos que averigüéis cual es pero solo se la dirá a aquel que conozca la palabra clave.",
         respuesta:"4728",
         mensaje:"",
     
