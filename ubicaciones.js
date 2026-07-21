bc06ba940f09f057debeeca8092123951565493e
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-21 12:15:45 +0200

    Update ubicaciones.js

--- ubicaciones.js
+++ ubicaciones.js
@@ -1,22 +1,17 @@
 const ubicaciones = {
 
-    "QR1": "Pádel",
-    "QR2": "Frontón",
-    "QR3": "Fuente",
-    "GRANDE1": "Prueba de las cuerdas",
-
-    "QR4": "Cementerio",
-    "QR5": "Ayuntamiento",
-    "QR6": "Parque",
-
-    "GRANDE2": "Prueba del médico",
-
-    "QR7": "Iglesia",
-    "QR8": "Báscula",
-    "QR9": "Huerto",
-
-    "GRANDE3": "Laboratorio",
-
-    "FINAL": "Hospital"
+    "QR1": "Padel",
+    "QR2": "Abuela Judith",
+    "QR3": "Bea",
+    "GRANDE1": "Centro Social",
+    "QR4": "Eloy",
+    "QR5": "Iglesia",
+    "QR6": "Castillo",
+    "GRANDE2": "Ambulancia",
+    "QR7": "Cementerio",
+    "QR8": "Bascula",
+    "QR9": "Buscavieja",
+    "GRANDE3": "Loco atado",
+    "FINAL": "Plaza Olivo"
 
 };
