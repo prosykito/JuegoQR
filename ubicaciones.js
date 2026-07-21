2293f0d2fa747da0db1ad644d4b7f10ab4f3096b
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-21 12:02:46 +0200

    Create ubicaciones.js

---
+++ ubicaciones.js
@@ -0,0 +1,22 @@
+const ubicaciones = {
+
+    "QR1": "Pádel",
+    "QR2": "Frontón",
+    "QR3": "Fuente",
+    "GRANDE1": "Prueba de las cuerdas",
+
+    "QR4": "Cementerio",
+    "QR5": "Ayuntamiento",
+    "QR6": "Parque",
+
+    "GRANDE2": "Prueba del médico",
+
+    "QR7": "Iglesia",
+    "QR8": "Báscula",
+    "QR9": "Huerto",
+
+    "GRANDE3": "Laboratorio",
+
+    "FINAL": "Hospital"
+
+};
