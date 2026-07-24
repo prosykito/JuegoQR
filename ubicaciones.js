55214098d5498e61f60d11f167c06d3e77d94b5a
prosykito <34734151+prosykito@users.noreply.github.com>
2026-07-24 22:26:20 +0200

    Update ubicaciones.js

--- ubicaciones.js
+++ ubicaciones.js
@@ -11,16 +11,18 @@ const ubicaciones = {
     "QR7": "Cementerio",
     "QR8": "Almacen",
     "QR9": "Trasera Santi",
-    "GRANDE3": "Loco atado",
+    "GRANDE3": "Compresor",
     "FINAL": "Plaza Olivo",
     "W24": "Abuela Judith",
     "G15": "Trasera Bea",    
     "R17": "Centro Social",
     "R23": "Iglesia",
     "U14": "Castillo",
+    "¡Ey, vosotros! He hackeado el sistema de los enfermeros y necesito vuestra ayuda. En uno de los traslados al psiquiátrico perdí mi silbato en la ambulancia y lo necesito con urgencia para poder continuar con mi meteórica carrera. Recordad que los enfermeros no recogen a cualquiera.": "Ambulancia", 
     "W34": "Cementerio, 
     "N18": "Almacen",
     "Z25": "Trasera Santi",
+    "W20": "Compresor"
     
     
 
